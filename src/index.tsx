acc319f3aa162e770999526710c9ce0fca01b45e
Lauri Kovanen <lkovanen@iki.fi>
2018-09-10 20:42:06 +0300

    Add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './components/App';
 import { unregister } from './registerServiceWorker';
 import './index.css';
+import { Provider, connect } from 'react-redux';
+import store from './js/store';
+
+import Slavemind from './components/slavemind/App';
+import Stars from './components/stars/App';
+
+interface Props {
+  appId: number | string;
+}
+
+const App = ({ appId }: Props) => {
+  switch (appId) {
+    case 1:
+    case 'stars':
+    return <Stars />;
+    default: 
+    return <Slavemind />;
+  }
+};
+
+const mapStateToProps = ({ appId }: Props) => ({ appId });
+const AppContainer = connect(mapStateToProps)(App);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+  <AppContainer />
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 unregister();
